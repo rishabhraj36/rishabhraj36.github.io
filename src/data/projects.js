91b4bab26cb2e7199924407b5300cda062f88eda
rshvraj <33702025+rshvraj@users.noreply.github.com>
2023-07-12 08:57:48 +0530

    new project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,10 +6,21 @@ export default function getProjects() {
         image:'Glossier.png',
         projectTitle: "Glossier",
         projectInfo:
-         "This is a International Cosmetic and Makeup E-commerce website for women",
+         "This is a International Cosmetic and Makeup E-commerce website for women. This was done by a team of 5 members over 4 days",
         techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
         projectLinkHosted: "https://glossier-ten.vercel.app/",
         projectLinkGithub: "https://github.com/rshvraj/Glossier",
+      },
+        {
+        img:"",
+        id: 2,
+        image:'Boat.png',
+        projectTitle: "Boat",
+        projectInfo:
+         "This is a clone of the popular website Boatlifestyle which sells speakers ,headphones and watches. This was done by a team of 4 over a period of 5 days",
+        techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
+        projectLinkHosted: "https://boat-lifestyle-kappa.vercel.app/",
+        projectLinkGithub: "https://github.com/Adarsh2102001/Boat_lifestyle",
       },
       
     ];
