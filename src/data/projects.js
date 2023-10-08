007b71b4af7524ac87b1b892d0a9d68bad90fab2
rshvraj <rrishabh.flutter@gmail.com>
2023-10-08 21:57:19 +0530

    project update order

--- src/data/projects.js
+++ src/data/projects.js
@@ -15,26 +15,16 @@ export default function getProjects() {
 
       {
         img:"",
-        id: 3,
-        image:'Glossier.png',
-        projectTitle: "Glossier",
+        id: 2,
+        image:'travelious.png',
+        projectTitle: "Travelious",
         projectInfo:
-         "This is a International Cosmetic and Makeup E-commerce website for women. This was done by a team of 5 members over 4 days",
-        techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
-        projectLinkHosted: "https://glossier-ten.vercel.app/",
-        projectLinkGithub: "https://github.com/rshvraj/Glossier",
+         "This is live website where I had worked on frontend and UI part",
+        techUsed: [  "ReactJS", "Chakra UI"],
+        projectLinkHosted: "https://my-app-nine-topaz-34.vercel.app/",
+        projectLinkGithub: "https://github.com/rshvraj/Travelious",
       },
-        {
-        img:"",
-        id: 5,
-        image:'Boat.png',
-        projectTitle: "Boat",
-        projectInfo:
-         "This is a clone of the popular website Boatlifestyle which sells speakers ,headphones and watches. This was done by a team of 4 over a period of 5 days",
-        techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
-        projectLinkHosted: "https://boat-lifestyle-kappa.vercel.app/",
-        projectLinkGithub: "https://github.com/Adarsh2102001/Boat_lifestyle",
-      },
+
       {
         img:"",
         id: 4,
@@ -46,17 +36,32 @@ export default function getProjects() {
         projectLinkHosted: "https://rshvraj.github.io/",
         projectLinkGithub: "https://github.com/rshvraj/rshvraj.github.io",
       },
+
       {
         img:"",
-        id: 2,
-        image:'travelious.png',
-        projectTitle: "Travelious",
+        id: 3,
+        image:'Glossier.png',
+        projectTitle: "Glossier",
         projectInfo:
-         "This is live website where I had worked on frontend and UI part",
-        techUsed: [  "ReactJS", "Chakra UI"],
-        projectLinkHosted: "https://my-app-nine-topaz-34.vercel.app/",
-        projectLinkGithub: "https://github.com/rshvraj/Travelious",
+         "This is a International Cosmetic and Makeup E-commerce website for women. This was done by a team of 5 members over 4 days",
+        techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
+        projectLinkHosted: "https://glossier-ten.vercel.app/",
+        projectLinkGithub: "https://github.com/rshvraj/Glossier",
       },
+
+        {
+        img:"",
+        id: 5,
+        image:'Boat.png',
+        projectTitle: "Boat",
+        projectInfo:
+         "This is a clone of the popular website Boatlifestyle which sells speakers ,headphones and watches. This was done by a team of 4 over a period of 5 days",
+        techUsed: [ "JS","Bootstrap", "HTML", "CSS"],
+        projectLinkHosted: "https://boat-lifestyle-kappa.vercel.app/",
+        projectLinkGithub: "https://github.com/Adarsh2102001/Boat_lifestyle",
+      },
+   
+     
 
       
     ];
